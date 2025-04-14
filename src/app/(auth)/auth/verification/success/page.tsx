ea118072de884aed0e27a6365b1b8ca6c5205ee8
fullstack_dev <dongying9159@outlook.com>
2025-04-13 18:27:15 -0600

    fix: pagination ui

--- src/app/(auth)/auth/verification/success/page.tsx
+++ src/app/(auth)/auth/verification/success/page.tsx
@@ -15,7 +15,7 @@ export default function VerificationSuccess() {
   return (
     <div className="space-y-6">
       <div className="flex flex-col items-center space-y-4">
-        <CheckCircle className="h-16 w-16 text-success-6" />
+        <CheckCircle className="h-16 w-16 text-success-5" />
         <h2 className="text-xl font-semibold text-system-13">Autenticação</h2>
         <p className="text-center text-md text-system-12">
           Parabéns! Autenticação efetuada com sucesso!
