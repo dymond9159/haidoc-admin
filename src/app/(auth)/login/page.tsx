6e8c6d6596b7ccfc086a1d372bd70473bc0da6e1
fullstack_dev <dongying9159@outlook.com>
2025-04-28 16:19:52 -0600

    refactor: simplify login page layout and rename component to AdminLoginPage

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,21 +1,11 @@
 "use client"
 
-import type React from "react"
-import { LoginBenefits, LoginForm } from "@/components/auth"
+import { LoginForm } from "@/components/auth"
 
-export default function LoginPage() {
+export default function AdminLoginPage() {
   return (
-    <div className="w-full h-screen overflow-y-auto">
-      <div className="flex w-full h-full flex-col items-center md:flex-row">
-        <div className="flex-1 w-full h-full md:w-1/2 flex justify-center items-center md:px-10">
-          <div className="w-full space-y-8 p-6 py-20 sm:p-8 max-w-sm md:max-w-md md:rounded-lg md:border md:shadow-sm dark:border-gray-700 dark:bg-gray-800">
-            <LoginForm />
-          </div>
-        </div>
-        <div className="w-full h-full md:w-1/2">
-          <LoginBenefits />
-        </div>
-      </div>
+    <div className="w-full max-w-md space-y-8 rounded-lg bg-system-1 p-8 shadow-md">
+      <LoginForm />
     </div>
   )
 }
